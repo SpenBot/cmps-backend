c29daf6a3a6fb6ddfd6728e754653043a5377c01
superbuggy <jpqreichard@gmail.com>
2017-10-18 16:42:20 -0400

    add james' comments

--- db/models.js
+++ db/models.js
@@ -1,22 +1,24 @@
+// Really like the way you all have broken out each piece of functionality into separate files, and honored separation of concerns
 
-// DEPENDENCIES //
-// require database connection //
+
+// DEPENDENCIES
+// require database connection 
 const mongoose = require('./connection.js')
 
-//// requiring our exported schemas ////
+//// requiring our exported schemas
 const Schema = require('./schema.js')
 
-// CREATE MODELS //
+// CREATE MODELS
 
-// grab individual schemas that were exported //
+// grab individual schemas that were exported
 const UserSchema = Schema.UserSchema
 
-// define model, using a schema //
+// define model, using a schema
 const User = mongoose.model("User", UserSchema)
 
-// EXPORT MODULE //
+// EXPORT MODULE
 
-//export models //
+//export models
 module.exports = {
   User
 }
--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-// SERVER CONFIGURATION //
+// If you like the symmetical appearance of comment delimiters, use the format below:
+/* SERVER CONFIGURATION */
 
-// require external module dependencies //
+
+/* require external module dependencies */
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-// require models from models.js //
+// require models from models.js
 const Models = require('./db/models.js')
 
-// require database connection module //
+// require database connection module
 const mongoose = require('./db/connection.js')
 
-// run app as an express application //
+// run app as an express application
 const app = express();
 
-//enable use of static files, which is in a folder named public //
+//enable use of static files, which is in a folder named public
 app.use(express.static(__dirname + '/public'))
 
-// handles json post requests (needed for AJAX requests with JSON bodies) //
+// handles json post requests (needed for AJAX requests with JSON bodies)
 app.use(bodyParser.json({ extended: true }))
-// handles form submissions //
-// app.use(bodyParser.urlencoded({ extended: true }))
 
-// use cors //
+// use cors
 app.use(cors())
 
-// RUN SERVER //
+// RUN SERVER
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("\n\tServer active. Listening on port 4000\n")
 })
 
-// ROUTES //
+// ROUTES
 
-// grab each model, from exported models //
+// grab each model, from exported models
 const User = Models.User
+
+//Neither of these models are being used for queries, so I think you could remove them for the time being, especially since the models aren't being exported, and the values below will be undefined
 const Theater = Models.Theater
 const Movie = Models.Movie
 
-// USERS ROUTES //
+// USERS ROUTES
 
-// get all users //
+// get all users
 app.get('/api/users', (req, res) => {
   User.find()
     .then((users) => {
@@ -51,25 +53,25 @@ app.get('/api/users', (req, res) => {
     })
 })
 
-// get one user //
+// get one user
 app.get("/api/users/:username", (req, res) => {
   User.findOne({username: req.params.username}).then(function(user){
     res.json(user)
-    });
   });
+});
 
-// create user //
+// create user
 app.post("/api/users", (req, res) => {
   User.create(req.body).then(user => {
     console.log("post api/users", req.body);
     console.log("user:", user)
-    res.json('/users/'+ user.username)
+    res.json('/users/'+ user.username) //remove this, there should only be one possible response from an express router handler method
     res.json(user)
 
   });
 });
 
-// update user //
+// update user
 app.put('/api/users/:username', (req, res) => {
   User.findOneAndUpdate({username: req.params.username}, req.body, {new: true}).then(user => {
     res.json(user)
@@ -77,10 +79,14 @@ app.put('/api/users/:username', (req, res) => {
   console.log(`Put method called ${req}`)
 })
 
-// delete user //
-app.delete("api/users/:username/delete", (req, res) => {
+// delete user
+
+//I'd consider removing /delete from the path, since only a delete request will trigger this route, it's redundant
+
+app.delete("/api/users/:username/delete", (req, res) => { //I added a leading / here, the route was not functional without it
   User.findOneAndRemove({username: req.params.username})
-    .then(() => {
-      res.json("/users")
+    .then(() => { //I'd recommend passing in `user` to the parameter of this anonymous function and then responding with the deleted user. That way, on your front-end you could extract information from the response to display a message like `${username} was deleted`, should you have the need to. Otherwise, see comment below
+    console.log(thing);
+      res.json("/users") //not a valid JSON response, could also {success: true}
     })
 })
